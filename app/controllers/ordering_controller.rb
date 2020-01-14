25b7a2efc74f3cf6fa89e0d0fc03320ca1d553ce
Timothy Indyk <tim@undergroundpowers.com>
2020-01-14 01:29:52 -0500

    Adds new drink view and sends to store.

--- app/controllers/ordering_controller.rb
+++ app/controllers/ordering_controller.rb
@@ -10,12 +10,23 @@ class OrderingController < ApplicationController
   end
 
   def new
-
+    @order = Order.new
   end
 
   def create
-
+  @order = Order.new(order_params)
+  @order.sentToStore = true
+  if @order.save
+    redirect_to(ordering_index_path)
+  else
+  puts "We didn't save."
   end
+end
   
+
+private
+def order_params
+  params.require(:order).permit(:item_name, :size, :extraEspressoShots, :milkType, :flavor, :decaf, :sentToStore)
+end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,5 @@ Rails.application.routes.draw do
 root "ordering#index"
 
 resources :ordering
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
